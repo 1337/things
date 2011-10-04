a7b78516b61a2d1a44ee23978df0fdb6233a2cd5
Brian <b2lai@uwaterloo.ca>
2011-10-04 16:25:44 -0400

    fixed mysql without proper connection

---
+++ .things.php
@@ -0,0 +1,7 @@
+<?php
+    /*  dummy to make all includes path-independent
+        all controls need to do in this path is "include_once ('.things.php');" */
+     
+    include_once ('things/.things.php');
+
+?>
